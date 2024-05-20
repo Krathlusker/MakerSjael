23a2dc77a409c63f26c8b6b50540b200ea4d7334
Frederik Krath Nielsen <Frederikkrath@gmail.com>
2024-05-20 17:29:17 +0200

    Added image zoom system

---
+++ JS/imagefull.js
@@ -0,0 +1,11 @@
+//imagefull.js
+function getPics() {} //just for this demo
+const imgs = document.querySelectorAll('.full img');
+const fullPage = document.querySelector('#fullpage');
+
+imgs.forEach(img => {
+  img.addEventListener('click', function() {
+    fullPage.style.backgroundImage = 'url(' + img.src + ')';
+    fullPage.style.display = 'flex';
+  });
+});
